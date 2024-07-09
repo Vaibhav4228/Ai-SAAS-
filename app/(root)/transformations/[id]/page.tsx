1bd63594cee530fcfe2ab98ade2738f6f5962153
Vaibhav4228 <vaibhavsharma88890@gmail.com>
2024-07-09 20:36:08 +0530

    Your commit message

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TransformationsPage = () => {
+  return (
+    <div>
+      TransformationsPage
+
+    </div>
+  )
+}
+
+export default TransformationsPage
+
+//this transformations doest have a route like - localhost:3000/transformations
+//btw this have a id where we can use any route - localhost:3000/transformation/{id} -- which is dynamic route
---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddTransformatioTypePage = () => {
+  return (
+    <div>
+      AddTransformatioTypePage
+    </div>
+  )
+}
+
+export default AddTransformatioTypePage
